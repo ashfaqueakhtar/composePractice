f4756e83724547d46b00c9ab2960d67b4a7b9416
ashfaqueakhtar <ashfaquea.akhtar@indusnet.co.in>
2025-02-11 11:12:27 +0530

    more changes

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,25 +1,21 @@
 package com.example.firstcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -28,7 +24,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -87,24 +82,41 @@ fun Greeting(name: String) {
 
 @Composable
 fun InputField() {
-    val  state = remember{mutableStateOf("")}
+    val state = remember { mutableStateOf("") }
     TextField(
         value = state.value,
         onValueChange = {
-        state.value = it
-                        },
+            state.value = it
+        },
         label = {
-        Text("Hint Text")
-    })
+            Text("Hint Text")
+        })
 }
 
-@Preview(showBackground = true /*widthDp = 200, heightDp = 200*/)
+@Composable
+fun ColumnLayout() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("A")
+        Text("B")
+        Text("C")
+    }
+}// Similar is row
+
+@Composable
+fun BoxLayout() {// its like Relative or frame layout
+    Box(contentAlignment = Alignment.Center) {
+        Text("A")
+        Text("B")
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 200, heightDp = 200)
 @Composable
 fun GreetingPreview() {
-    /*FirstComposeTheme {
-        Greeting("Android")
-    }*/
-    //PreviewFunction()
-    //Submit()
-    InputField()
+
+    BoxLayout()
 }
